9a9a3e959d518770067189ebb6a1d369140e3669
Brayan <brayanMuniz2636@gmail.com>
2021-05-19 14:09:50 -0500

    Generates Dates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,13 @@
     <button @click="signOut">Sign Out</button>
 
     <div>Chart</div>
-    <video v-if="videoReady" ref="videoPlayer">
-      <source :src="videoUrl" />
-    </video>
-    <button @click="playVid" type="button">Play Vid</button>
-    <button @click="pauseVid" type="button">Pause Vid</button>
+    <div v-if="videoReady">
+      <video ref="videoPlayer">
+        <source :src="videoUrl" />
+      </video>
+      <button @click="playVid" type="button">Play Vid</button>
+      <button @click="pauseVid" type="button">Pause Vid</button>
+    </div>
 
     <div>Stats</div>
   </div>
@@ -16,6 +18,7 @@
 <script lang="ts">
 import Vue from "vue";
 import { firebaseApp } from "@/firebase";
+import moment from "moment";
 
 export default Vue.extend({
   name: "Home",
@@ -26,20 +29,23 @@ export default Vue.extend({
     };
   },
   async mounted() {
+
+
     // this is the actual name of the video NOT the url link
-    const testVideo = "redditsave.com_numa_numa-t8vuqusl0fm61.mp4";
-    let testVideRef = firebaseApp.storage().ref(testVideo);
-    await testVideRef
-      .getDownloadURL()
-      .then((url) => {
-        this.videoUrl = url;
-        this.videoReady = true;
-      })
-      .catch((err) => {
-        this.videoUrl = "";
-        this.videoReady = false;
-        console.error(err);
-      });
+    this.retriveWorkoutData();
+    // const testVideo = "redditsave.com_numa_numa-t8vuqusl0fm61.mp4";
+    // let testVideRef = firebaseApp.storage().ref(testVideo);
+    // await testVideRef
+    //   .getDownloadURL()
+    //   .then((url) => {
+    //     this.videoUrl = url;
+    //     this.videoReady = true;
+    //   })
+    //   .catch((err) => {
+    //     this.videoUrl = "";
+    //     this.videoReady = false;
+    //     console.error(err);
+    //   });
   },
   methods: {
     async signOut() {
@@ -52,6 +58,32 @@ export default Vue.extend({
         .catch((err) => {
           console.error(err);
         });
+    },
+    generateArrayOfDates(startDate: string, endDate: string): Array<string> {
+      let dates: Array<string> = [];
+      let startDateMoment = moment(startDate);
+      let endDateMoment = moment(endDate);
+
+      // With +1 it will include the day of
+      const diffInDays: number =
+        endDateMoment.diff(startDateMoment, "days") + 1;
+
+      // Populates array with dates
+      for (let i = 0; i < diffInDays; i++) {
+        let calculatedDay = moment(startDate).add(i, "days");
+        dates.push(moment(calculatedDay).format("MM-DD-YYYY"));
+      }
+      console.log(dates);
+      return dates;
+    },
+    // if startDate not provided, gets data from one week ago to now
+    async retriveWorkoutData(startDate?: string, endDate?: string) {
+      let dates: Array<string> = this.generateArrayOfDates(
+        "05/10/2021",
+        "05/14/2021"
+      );
+      firebaseApp.firestore().collection("users").doc()
+
     },
     playVid() {
       this.$refs["videoPlayer"].play();

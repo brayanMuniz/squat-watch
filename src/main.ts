a31b9789185079f4d8d6c6cace7b9f4aad610bc2
Brayan <brayanMuniz2636@gmail.com>
2021-05-28 13:32:29 -0500

    gets and sets uid then renders app.vue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import firebaseApp from "firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let app: any = null;
+
+firebaseApp.auth().onAuthStateChanged(async (user) => {
+  if (user) {
+    store.commit("updateMyUID", user.uid);
+  } else {
+    store.commit("updateMyUID", "");
+  }
+
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount("#app");
+  }
+});

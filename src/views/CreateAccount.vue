d434fa8390fe3d2cdb4c3907a1ecb7ce531fbb21
Brayan <brayanMuniz2636@gmail.com>
2021-06-17 14:01:52 -0500

    Added more options when creating account

--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,6 +1,11 @@
 <template>
   <form @submit.prevent="makeNewUser">
     <div class="form-group">
+      <label for="Email">User Name</label>
+      <input type="text" class="form-control" v-model.trim="userName" />
+    </div>
+
+    <div class="form-group">
       <label for="Email">Email address</label>
       <input
         type="email"
@@ -20,41 +25,41 @@
         id="Password"
       />
     </div>
+    <!-- Public, Friends Only, Private -->
+    <select
+      class="form-select"
+      aria-label="Default select example"
+      v-model="visibility"
+    >
+      <option value="Public">Public</option>
+      <option value="Friends Only">Friends Only</option>
+      <option value="Private">Private</option>
+    </select>
 
-    <!-- Lifts  -->
-    <div class="form-group">
-      <label for="Password">Squat </label>
-      <input
-        v-model.trim="initialLifts.Squat"
-        type="number"
-        class="form-control"
-        id="Password"
-      />
-    </div>
+    <!--Initial Lifts  -->
+    <div>Current Poggers</div>
+    <div
+      class="form-group"
+      v-for="(exercise, index) in initialLiftsData"
+      :key="index"
+    >
+      <button @click="removeLift(index)" type="button">-R</button>
 
-    <div class="form-group">
-      <label for="Password">Bench</label>
-      <input
-        v-model.trim="initialLifts.Bench"
-        type="number"
-        class="form-control"
-        id="Password"
-      />
-    </div>
+      <!-- Todo: Add a dropdown of suggested lifts, Squat, Bench, Deadlift etx  -->
+      <label>Exercise Name</label>
+      <input v-model="exercise.name" type="text" />
 
-    <div class="form-group">
-      <label for="Password">Deadlift</label>
-      <input
-        v-model.trim="initialLifts.Deadlift"
-        type="number"
-        class="form-control"
-        id="Password"
-      />
+      <label>Weight </label>
+      <input v-model="exercise.weight" type="number" class="form-control" />
+
+      <label>Reps </label>
+      <input v-model="exercise.reps" type="number" class="form-control" />
     </div>
+    <button @click="addNewExercise" type="button">Add New Exercise</button>
 
     <!-- Upload a profile image -->
     <div class="form-group">
-      <label for="Starting Amount">Profile Image:</label>
+      <label for="Starting Amount">Profile Picture :</label>
       <input
         ref="upload"
         name="file-upload"
@@ -77,21 +82,34 @@ import moment from "moment";
 export default Vue.extend({
   data() {
     return {
+      userName: "", // Todo: make sure userName is not taken
       email: "",
       password: "",
+      visibility: "Public",
       profileImageUrl: "",
-      initialLifts: {
-        Squat: 0,
-        Bench: 0,
-        Deadlift: 0,
-      },
+      initialLiftsData: [{ name: "Squat", weight: 0, reps: 5 }],
       profileImage: new Blob(),
       userWantsProfileImage: false,
     };
   },
   methods: {
     async makeNewUser() {
-      if (this.password.length < 6) {
+      let userNameTakenCondition = false;
+      let checkingFirebaseError = false;
+      await this.isUserNameTaken()
+        .then((userNameTaken: boolean) => {
+          userNameTakenCondition = userNameTaken;
+        })
+        .catch((err) => {
+          checkingFirebaseError = true;
+          console.error(err);
+        });
+
+      if (checkingFirebaseError) {
+        alert("Problem on our side");
+      } else if (userNameTakenCondition) {
+        alert("User Name is taken");
+      } else if (this.password.length < 6) {
         alert("Make sure your password is at least 6 characters");
       } else {
         console.log("Making new user...");
@@ -121,6 +139,11 @@ export default Vue.extend({
 
               await this.addUserToFireStore(res.user?.uid)
                 .then(() => {
+                  this.addUserNameToFireStore(this.userName, {
+                    belongsTo: res.user?.uid
+                  }).then(res => {
+                    console.log("User Name was added to firestore")
+                  })
                   console.log("User was added to firestore");
                 })
                 .catch((err) => {
@@ -137,7 +160,9 @@ export default Vue.extend({
     async addUserToFireStore(userUID: string) {
       let initalUserData: any = {
         dateJoined: moment().format("MM-DD-YYYY"),
-        initialLifts: this.initialLifts,
+        initialLifts: this.initialLiftsData,
+        userName: this.userName,
+        visibility: this.visibility,
       };
       if (this.profileImageUrl !== "" || this.profileImageUrl !== undefined)
         initalUserData["profileImageUrl"] = this.profileImageUrl;
@@ -147,6 +172,40 @@ export default Vue.extend({
         .collection("users")
         .doc(userUID)
         .set(initalUserData);
+    },
+    async addUserNameToFireStore(userName: string, docData: any) {
+      await firebaseApp
+        .firestore()
+        .collection("userNames")
+        .doc(userName)
+        .set(docData);
+    },
+    async isUserNameTaken() {
+      let userNameTaken = false;
+      let errorGettingName = false;
+      let errData: any = {};
+      await firebaseApp
+        .firestore()
+        .collection("userNames")
+        .doc(this.userName)
+        .get()
+        .then((res) => {
+          if (res.exists) userNameTaken = true;
+        })
+        .catch((err) => {
+          errData = err;
+          errorGettingName = true;
+        });
+      if (errorGettingName) return Promise.reject(errData);
+      return Promise.resolve(userNameTaken);
+    },
+    addNewExercise() {
+      this.initialLiftsData.push({ name: "", weight: 0, reps: 0 });
+    },
+    removeLift(idx: number) {
+      if (this.initialLiftsData[idx]) {
+        this.initialLiftsData.splice(idx, 1);
+      }
     },
     previewFiles(event: any): void {
       if (event.target.files[0]) {

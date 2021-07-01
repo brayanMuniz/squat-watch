ec1e5a6a68d5c7fa5d9f175905a20af4cbbfef65
Brayan <brayanMuniz2636@gmail.com>
2021-06-30 23:56:20 -0500

    Removed white space and more crunched together

---
+++ src/views/History.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+// Todo: make Workout card into component
+// Essentially copy history from app and disaply it here
+// On Desktop figure out how to add more space || remove it completly and make it mobile only
+export default Vue.extend({
+  name: "History",
+});
+</script>

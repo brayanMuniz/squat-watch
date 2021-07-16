892cd4e62bb8e6185895c5a1fcd44c64aa7270d2
Brayan <brayanMuniz2636@gmail.com>
2021-07-16 11:01:44 -0500

    Fixed breakpoints on /browse

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -1,45 +1,46 @@
 <template>
   <div>
     <Navbar />
+    <div class="container-fluid">
+      <div class="row row-cols-1 row-cols-md-3">
+        <div class="col" v-for="(user, idx) in usersData" :key="idx">
+          <div class="card">
+            <router-link
+              :to="{
+                name: 'BrowseUser',
+                params: {
+                  userName: user.userName,
+                  userData: user.data,
+                  userUID: user.uid,
+                },
+              }"
+            >
+              <img
+                v-if="user.data.profileImageUrl"
+                :src="user.data.profileImageUrl"
+                class="card-img-top"
+                alt="userProfileImage"
+            /></router-link>
 
-    <div class="row row-cols-sm-1 row-cols-md-3">
-      <div class="col" v-for="(user, idx) in usersData" :key="idx">
-        <div class="card">
-          <router-link
-            :to="{
-              name: 'BrowseUser',
-              params: {
-                userName: user.userName,
-                userData: user.data,
-                userUID: user.uid,
-              },
-            }"
-          >
-            <img
-              v-if="user.data.profileImageUrl"
-              :src="user.data.profileImageUrl"
-              class="card-img-top"
-              alt="userProfileImage"
-          /></router-link>
-
-          <div class="card-body">
-            <h5 class="card-title">
-              <router-link
-                :to="{
-                  name: 'BrowseUser',
-                  params: {
-                    userName: user.userName,
-                    userData: user.data,
-                    userUID: user.uid,
-                  },
-                }"
-                >{{ user.data.userName }}</router-link
-              >
-            </h5>
-            <h6 class="card-subtitle mb-2 text-muted text-truncate">
-              {{ user.data.description }}
-            </h6>
-            <p class="card-text"></p>
+            <div class="card-body">
+              <h5 class="card-title">
+                <router-link
+                  :to="{
+                    name: 'BrowseUser',
+                    params: {
+                      userName: user.userName,
+                      userData: user.data,
+                      userUID: user.uid,
+                    },
+                  }"
+                  >{{ user.data.userName }}</router-link
+                >
+              </h5>
+              <h6 class="card-subtitle mb-2 text-muted text-truncate">
+                {{ user.data.description }}
+              </h6>
+              <p class="card-text"></p>
+            </div>
           </div>
         </div>
       </div>
